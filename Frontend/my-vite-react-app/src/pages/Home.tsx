47631e6ca4e3745bab0ae2749e3f63f80f105dea
Kacper <kacperodziemczyk@gmail.com>
2024-09-04 12:37:51 +0200

    add whole routing

---
+++ Frontend/my-vite-react-app/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/Home.css'
+
+const Home = () => {
+  return (
+    <main className="home">
+      <h2 className="title">Witaj w naszym sklepie!</h2>
+      <p className="description">Znajdź najlepsze produkty w najlepszych cenach.</p>
+    </main>
+  )
+}
+
+export default Home
